f9298f8cf243aa72fe3ece9fb17b43ce20f9cc25
Eric Pricop <dimitripricop@yahoo.com>
2018-02-16 20:53:45 -0800

    survey project

---
+++ surveys_project/apps/surveys_app/views.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+
+def index(request):
+	return render(request, 'index.html')
+
+def result(request):
+	print request.session['context']
+	return render(request, 'result.html')
+def process(request):
+	if len(request.POST['name']) < 1:
+		messages.warning(request,'Name Cannot Be Empty!')
+		return redirect('/')
+	if len(request.POST['comment']) > 120:
+		messages.warning(request,'Comment should be less than 120 characters!')
+		return redirect('/')
+	if 'times' in request.session:
+		request.session['times'] += 1
+	else:
+		request.session['times']=1
+	context = {
+	'name': request.POST['name'],
+	'location': request.POST['location'], 
+	'language': request.POST['language'],
+	'comment': request.POST['comment']
+	}
+	request.session['context'] = context
+
+	return redirect('/result')
+
+# Create your views here.
